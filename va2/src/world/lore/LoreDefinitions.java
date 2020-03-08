d7fb18730f335cab344308fe3bcdd64332bf59b7
VectorShadow <ncbrayshaw@hotmail.com>
2020-03-09 02:42:58 +0430

    Lore reworded.

--- va2/src/world/lore/LoreDefinitions.java
+++ va2/src/world/lore/LoreDefinitions.java
@@ -15,16 +15,17 @@ public class LoreDefinitions {
                     .addTheme("General")
                         .addLeaf(
                                 "New Player Message",
-                                "\t\"The most merciful thing in the world, I think, is the inability of the human " +
-                                        "mind to correlate all its contents. We live on a placid island of ignorance " +
-                                        "in the midst of black seas of infinity, and it was not meant that we should " +
-                                        "voyage far. The sciences, each straining in its own direction, have hitherto " +
-                                        "harmed us little; but some day the piecing together of dissociated knowledge " +
-                                        "will open up such terrifying vistas of reality, and of our frightful " +
-                                        "position therein, that we shall either go mad from the revelation or flee " +
-                                        "from the deadly light into the peace and safety of a new dark age.\"" +
+                                "\t\"The most merciful thing in the world, I think, is the inability of the " +
+                                        "human mind to correlate all its contents. We live on a placid island of " +
+                                        "ignorance in the midst of black seas of infinity, and it was not meant that " +
+                                        "we should voyage far. The sciences, each straining in its own direction, " +
+                                        "have hitherto harmed us little; but some day the piecing together of " +
+                                        "dissociated knowledge will open up such terrifying vistas of reality, and " +
+                                        "of our frightful position therein, that we shall either go mad from the " +
+                                        "revelation or flee from the deadly light into the peace and safety of a " +
+                                        "new dark age.\"" +
                                         "\n" +
-                                        "\n-H. P. Lovecraft, in The Call of Cthulhu" +
+                                        "\n-H. P. Lovecraft (The Call of Cthulhu)" +
                                         "\n" +
                                         "\n\n\tThis appears to be your first time playing Chronicles of the Abyss on " +
                                         "this computer. " +
