72159e137803fd64d9e133b6578b29c775153d5a
VectorShadow <ncbrayshaw@hotmail.com>
2020-04-09 01:01:38 +0430

    Refactored XP Scale by level tiers.

--- va2/src/main/progression/rewards/Experience.java
+++ va2/src/main/progression/rewards/Experience.java
@@ -4,7 +4,7 @@ import main.extensible.Saveable;
 
 public class Experience extends Saveable {
     private static final long LEVEL_ONE = 64;
-    private static final int MAX_LEVEL = 127;
+    private static final int MAX_LEVEL = 255;
     private static long[] XP_TABLE = new long[MAX_LEVEL + 1];
 
     private long exp = 0;
@@ -46,13 +46,24 @@ public class Experience extends Saveable {
         }
     }
     private static double slidingScale(int level){
-        return 1.0 + (1.0 / (Math.sqrt(Math.E) * Math.log(level)));
+        return 1.0 +
+                (
+                        (
+                                (41.0 / 64.0) + //set a base rate
+                                        ((double)(level / 0x0f) / 32.0) + //accelerate rate of increase every 16 levels
+                                        ((double)(level / 0x50) / 16.0) + //and ramp up the acceleration after level 80
+                                        ((double)(level / 0xa0) / 4.0) + //and again after level level 160
+                                        ((double)(level / 0xf0) / 0.5) //and finally once more after level 240
+                        ) /
+                                (Math.sqrt(Math.E) * Math.log(level)) //apply a logarithmic scale of increase
+                );
     }
     public static void fillXPTable() {
         XP_TABLE[0] = 0;
         XP_TABLE[1] = LEVEL_ONE;
         for (int i = 2; i < XP_TABLE.length; ++i) {
             XP_TABLE[i] = (long)((double)XP_TABLE[i-1] * slidingScale(i));
+            System.out.println(i + ": " + XP_TABLE[i]);
         }
     }
     public static long calculateXP(int level) {
